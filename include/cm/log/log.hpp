8c81ec021f99be40e471368f535ea2d3e5798426
Alexandr Esilevich <esil0x@gmail.com>
2024-08-06 03:57:04 +0100

    fixed logging filtering by category/subcategory

--- include/cm/log/log.hpp
+++ include/cm/log/log.hpp
@@ -9,22 +9,60 @@
 
 #include <boost/log/trivial.hpp>
 #include <boost/log/utility/manipulators/add_value.hpp>
+#include <boost/log/attributes/constant.hpp>
 
 
 namespace cm::log {
 
 
+template <auto N>
+struct log_attr_string {
+    constexpr log_attr_string(const char (&s)[N]) {
+        std::copy_n(s, N, value);
+    }
+
+    /// Returns string value containing attribute
+    std::string string() const {
+        return std::string{&value[0], sizeof(value) - 1};
+    }
+
+    char value[N];
+};
+
+
 using logger_t = boost::log::sources::severity_logger_mt<boost::log::trivial::severity_level>;
 
-/// Returns reference to logger
+
+/// Returns reference to logger with category attribute
+template <log_attr_string Category>
 inline logger_t & get_logger() {
-    static logger_t logger;
+    auto init_logger = [] {
+        logger_t logger;
+        logger.add_attribute("Category", boost::log::attributes::make_constant(Category.string()));
+        return logger;
+    };
+    static logger_t logger = init_logger();
     return logger;
 }
 
+
+/// Returns reference to logger with category and subcategory attributes
+template <log_attr_string Category, log_attr_string Subcategory>
+inline logger_t & get_logger_scat() {
+    auto init_logger = [] {
+        logger_t logger;
+        logger.add_attribute("Category", boost::log::attributes::make_constant(Category.string()));
+        logger.add_attribute("Subcategory",
+                             boost::log::attributes::make_constant(Subcategory.string()));
+        return logger;
+    };
+    static logger_t logger = init_logger();
+    return logger;
+}
+
+
 #define CM_LOG(cat, level) \
-    BOOST_LOG_SEV(::cm::log::get_logger(), level) \
-        << ::boost::log::add_value("Category", #cat)
+    BOOST_LOG_SEV((::cm::log::get_logger<#cat>()), level)
 
 #define CM_LOG_TRACE(cat)       CM_LOG(cat, ::boost::log::trivial::trace)
 #define CM_LOG_DEBUG(cat)       CM_LOG(cat, ::boost::log::trivial::debug)
@@ -35,9 +73,7 @@ inline logger_t & get_logger() {
 
 
 #define CM_LOG_SCAT(cat, scat, level) \
-    BOOST_LOG_SEV(::cm::log::get_logger(), level) \
-        << ::boost::log::add_value("Category", #cat) \
-        << ::boost::log::add_value("Subcategory", #scat)
+    BOOST_LOG_SEV((::cm::log::get_logger_scat<#cat, #scat>()), level)
 
 #define CM_LOG_SCAT_TRACE(cat, scat)    CM_LOG_SCAT(cat, scat, ::boost::log::trivial::trace)
 #define CM_LOG_SCAT_DEBUG(cat, scat)    CM_LOG_SCAT(cat, scat, ::boost::log::trivial::debug)
