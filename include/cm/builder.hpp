11e40eb8313e59f1eb7d76f482fadc0ab9527d04
Alexandr Esilevich <esil0x@gmail.com>
2026-07-29 23:29:51 +0100

    removed UTF BOM header from builder.hpp

--- include/cm/builder.hpp
+++ include/cm/builder.hpp
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2024, Alexandr Esilevich
+// Copyright (c) 2024, Alexandr Esilevich
 // 
 // Distributed under the BSD 2-Clause License.
 // See accompanying file LICENSE for license information.
